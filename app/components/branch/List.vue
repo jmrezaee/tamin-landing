60466e9613d0c89e26e55acfec3b866682f1b102
Mehrdad Rezaee <mehrdad.rz@gmail.com>
2026-04-21 21:18:04 +0330

    implemented merchants

--- app/components/branch/List.vue
+++ app/components/branch/List.vue
@@ -51,7 +51,7 @@ onNuxtReady(async () => {
   <div class="flex flex-col gap-4">
     <div class="flex flex-col gap-4 p-4 rounded-4xl bg-white">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        <div class="grid grid-cols-[auto_1fr] gap-2">
+        <div class="flex flex-row gap-2">
           <div
             class="flex justify-center items-center rounded-2xl size-14 bg-neutral-100 overflow-hidden"
           >
@@ -62,16 +62,14 @@ onNuxtReady(async () => {
             />
           </div>
           <div class="flex flex-col gap-2">
-            <span class="text-sm font-medium">
-              {{ shop_show?.display_name }}
-            </span>
+            <span class="text-sm font-medium">{{ shop_show?.name }}</span>
             <div class="flex flex-wrap flex-row gap-2">
               <span
                 class="text-xs text-neutral-500 font-light py-1 px-2 rounded-md bg-neutral-100"
-                v-for="(i, indx) in shop_show?.categories"
+                v-for="(i, indx) in shop_show?.tags"
                 :key="indx"
               >
-                {{ i.name }}
+                {{ i.title }}
               </span>
             </div>
           </div>
@@ -80,7 +78,9 @@ onNuxtReady(async () => {
           <span class="text-sm text-neutral-500 font-light">
             تعداد شعب فعال:
           </span>
-          <span class="font-bold"> {{ branch_meta?.total }} شعبه </span>
+          <span class="font-bold">
+            {{ branch_meta?.pagination.total }} شعبه
+          </span>
         </div>
         <div class="flex flex-col gap-2">
           <span class="text-sm text-neutral-500 font-light">
@@ -101,21 +101,20 @@ onNuxtReady(async () => {
     </div>
     <div class="flex flex-col gap-4 p-4 rounded-4xl bg-white">
       <div class="flex flex-row justify-between items-center gap-2">
-        <span class="font-bold"> لیست شعب ({{ branch_meta?.total }}) </span>
+        <span class="font-bold">
+          لیست شعب ({{ branch_meta?.pagination.total }})
+        </span>
       </div>
       <Table>
         <TableHeader>
           <TableRow class="border-0! bg-neutral-100">
-            <!-- <TableHead
+            <TableHead
               class="text-sm text-neutral-500 text-center rounded-r-xl"
             >
               استان
             </TableHead>
             <TableHead class="text-sm text-neutral-500 text-center">
               شهر
-            </TableHead> -->
-            <TableHead class="text-sm text-neutral-500 text-center">
-              نام شعبه
             </TableHead>
             <TableHead class="text-sm text-neutral-500 text-center">
               کد شعبه
@@ -132,9 +131,8 @@ onNuxtReady(async () => {
         </TableHeader>
         <TableBody>
           <TableRow v-for="(item, index) in branch_list" :key="index">
-            <!-- <TableCell class="text-center">{{ item.city.province }}</TableCell>
-            <TableCell class="text-center">{{ item.city.title }}</TableCell> -->
-            <TableCell class="text-center">{{ item.name }}</TableCell>
+            <TableCell class="text-center">{{ item.city.province }}</TableCell>
+            <TableCell class="text-center">{{ item.city.title }}</TableCell>
             <TableCell class="text-center">{{ item.code }}</TableCell>
             <TableCell class="text-center">{{ item.address }}</TableCell>
             <TableCell class="text-center">{{ item.phone }}</TableCell>
@@ -143,9 +141,9 @@ onNuxtReady(async () => {
       </Table>
       <Pagination
         v-slot="{ page }"
-        :items-per-page="branch_meta.per_page"
-        :total="branch_meta.total"
-        :default-page="branch_meta.current_page"
+        :items-per-page="branch_meta.pagination.per_page"
+        :total="branch_meta.pagination.total"
+        :default-page="branch_meta.pagination.current_page"
         v-if="branch_meta?.status"
       >
         <PaginationContent v-slot="{ items }">
