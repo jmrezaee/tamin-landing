6ccd50d79fd596af28c44d03cb76bc72295bfc6e
Mehrdad Rezaee <mehrdad.rz@gmail.com>
2026-04-21 15:48:23 +0330

    added refah logo

--- app/components/landing/Shops.vue
+++ app/components/landing/Shops.vue
@@ -25,7 +25,6 @@ const shop_items = ref([
     name: "دیلی مارکت",
     image: "/images/image-carousel-shop-3.webp",
   },
-<<<<<<< HEAD
   {
     name: "گندم",
     image: "/images/image-carousel-shop-2.webp",
@@ -34,12 +33,10 @@ const shop_items = ref([
     name: "رفاه",
     image: "/images/image-carousel-shop-7.webp",
   },
-=======
-  // {
-  //   name: "هایپرمی",
-  //   image: "/images/image-carousel-shop-4.webp",
-  // },
->>>>>>> 855af87 (fixed some)
+  {
+    name: "رفاه",
+    image: "/images/image-carousel-shop-7.webp",
+  },
   {
     name: "هفت",
     image: "/images/image-carousel-shop-5.webp",
